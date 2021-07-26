f4010bd37d63a0badab7cfc5525dacd47b0a0d92
Zipeng <zipengchen1832@gmail.com>
2021-07-25 17:31:26 -0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        List<Integer> nums = new ArrayList<>();
+        for(int i=0;i<5;i++){
+            System.out.print("Enter number "+(i+1)+": ");
+            nums.add(input.nextInt());
+        }
+
+        int sum = 0, max = nums.get(0), min=nums.get(0), product = 1;
+        for(int num : nums){
+            sum += num;
+            product *= num;
+            if(num>max)
+                max = num;
+            if(num<min)
+                min = num;
+        }
+
+        System.out.println("The sum of the numbers is "+sum);
+        System.out.println("The product of the numbers is "+product);
+        System.out.println("The largest number is "+max);
+        System.out.println("The smallest number is "+min);
+
+    }
+}
